fe09e0bc6578542ea4296517fb0f622648d40081
mncockerham <mncockerham@hotmail.com>
2026-03-12 18:09:27 -0400

    updated DBT process

--- DBT/list_tables.py
+++ DBT/list_tables.py
@@ -4,7 +4,7 @@ try:
     con.install_extension('postgres')
     con.load_extension('postgres')
     print("Attaching postgres_air...")
-    con.sql("ATTACH 'postgresql://postgres:ZAQ!2wsx@localhost:5432/postgres_air' AS pg (TYPE POSTGRES, READ_ONLY)")
+    con.sql("ATTACH 'postgresql://postgres@localhost:5432/postgres_air' AS pg (TYPE POSTGRES, READ_ONLY)")
     print("Attached. Querying tables...")
     res = con.sql("DESCRIBE pg.postgres_air.phone").fetchall()
     print("Tables found:", res)
--- DBT/target/compiled/my_dbt_project/models/phone_ppi_clean.sql
+++ DBT/target/compiled/my_dbt_project/models/phone_ppi_clean.sql
@@ -7,4 +7,4 @@ select
     phone_type,
     primary_phone,
     update_ts
-from postgres_scan('postgresql://postgres:ZAQ!2wsx@localhost:5432/postgres_air', 'postgres_air', 'phone')
+from postgres_scan('postgresql://postgres@localhost:5432/postgres_air', 'postgres_air', 'phone')
